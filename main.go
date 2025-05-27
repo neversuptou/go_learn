470bbba7d5e8b46221360c8f48f70fa76f97bd09
neversuptou <danilasereda2@yandex.ru>
2025-05-27 18:48:43 +0300

    Create app IMT calc

--- main.go
+++ main.go
@@ -1,21 +1,73 @@
 package main
 
 import (
-  "fmt"
+	"errors"
+	"fmt"
+	"math"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+const IMTPower = 2
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	for {
+		fmt.Println("__ Калькулятор индекса массы тела __ ")
+		userWeight, userHeight := getUserInput()
+		IMT, err := calculateIMT(userWeight, userHeight)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		outputResult(IMT)
+		if !breakeApp() {
+			break
+		}
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+}
+
+func outputResult(IMT float64) {
+	switch {
+	case IMT < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case IMT < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case IMT < 25:
+		fmt.Println("У вас нормальная масса тела")
+	case IMT < 30:
+		fmt.Println("У вас избыточная масса тела")
+	case IMT < 35:
+		fmt.Println("У вас 1-я степень ожирения")
+	case IMT < 40:
+		fmt.Println("У вас 2-я степень ожирения")
+	default:
+		fmt.Println("У вас 3-я степень ожирения")
+	}
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f\n", IMT)
+	fmt.Print(result)
+}
+
+func calculateIMT(userWeight float64, userHeight float64) (IMT float64, err error) {
+	if userWeight <= 0 || userHeight <= 0 {
+		return 0, errors.New("Не указан вес или рост")
+	}
+	IMT = userWeight / math.Pow(userHeight/100, IMTPower)
+	return IMT, nil
+}
+
+func getUserInput() (userHeight float64, userWeight float64) {
+	fmt.Println("Введите свой рост(см):")
+	fmt.Scan(&userHeight)
+	fmt.Println("Введите свой вес:")
+	fmt.Scan(&userWeight)
+	return userWeight, userHeight
+}
+
+func breakeApp() bool {
+	var answerForCalc string
+	fmt.Println("Хотите рассчитать индекс повторно? y/n")
+	fmt.Scan(&answerForCalc)
+	if answerForCalc == "y" || answerForCalc == "Y" {
+		return true
+	}
+	return false
 }
