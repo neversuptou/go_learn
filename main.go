0d8d34649dfec6c43d54d78de6d49b565b2f64f9
neversuptou <danilasereda2@yandex.ru>
2025-06-05 17:37:41 +0300

    first http

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/fatih/color"
+	"learnGO/geo"
 )
 
 func main() {
-	color.Green("Hello World")
+	city := flag.String("city", "", "City Name")
+	//format := flag.Int("format", 1, "Format output the weather")
+	flag.Parse()
+
+	fmt.Println(*city)
+	geo, err := geo.GetMyLocation(*city)
+	if err != nil {
+		color.Red(err.Error())
+		return
+	}
+	fmt.Println(*geo)
 }
