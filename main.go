07a61b599b3b71e50cc8d38a02368e8c32049d4f
neversuptou <danilasereda2@yandex.ru>
2025-06-05 12:20:05 +0300

    init app weather

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"github.com/fatih/color"
-	"github.com/joho/godotenv"
-	"learnGO/menu"
-	"learnGO/utils"
 )
 
 func main() {
-	color.Green("__Password manager__")
-	err := godotenv.Load()
-	if err != nil {
-		utils.PrintError("Error loading .env file")
-	}
-App:
-	for {
-		breaked := menu.ChooseMenu()
-		if breaked {
-			break App
-		}
-	}
+	color.Green("Hello World")
 }
